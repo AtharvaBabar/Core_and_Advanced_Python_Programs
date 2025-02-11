080e220ef96e5b7444484cc3514bf01b99fd53ff
Atharva Babar <157498667+AtharvaBabar@users.noreply.github.com>
2025-02-11 19:12:39 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,23 @@
+#Calculate the multiplication and sum of two numbers
+
+# declaring two numbers from user
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter seconnd number:"))
+
+'''Performing addition and multiplicaiton'''
+add = num_1 + num_2
+multiplication = num_1 * num_2
+print("addition of two number: ",add)
+print("multiplication of two numbers: ",multiplication)
+
+
+'''
+OUTPUT:
+
+Enter first number-: 5
+Enter seconnd number-:4
+addition of two number -  9
+multiplication of two numbers-  20
+
+'''
+
---
+++ Day2_Assignment2.py
@@ -0,0 +1,18 @@
+#Declare two variables and print that which variable is largest using ternary operators
+
+# Define two variables
+a = 15
+b = 20
+
+# Find the largest using a ternary operator
+largest = a if a > b else b
+
+# Output the largest variable
+print("Largest variable is:", largest)
+
+
+'''
+OUTPUT:
+
+Largest variable is: 20
+'''
